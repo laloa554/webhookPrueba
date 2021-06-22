3d0bdeadeb5c27bf6766de8f7158ba253a534352
laloa554 <55073002+laloa554@users.noreply.github.com>
2021-06-22 10:09:31 -0500

    Add files via upload

---
+++ webHook/src/test/java/com/webHook/WebHookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webHook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
